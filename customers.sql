492d03690876ab5c5ddeaa9f4b698dd41e801ac0
arther2945 <arther2945@gmail.com>
2025-04-13 02:14:46 +0900

    Create customers.sql

---
+++ customers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+  customer_no INT PRIMARY KEY AUTO_INCREMENT,
+  company_name VARCHAR(30),
+  contact_name VARCHAR(20),
+  contact_position VARCHAR(20),
+  address VARCHAR(50),
+  city VARCHAR(20),
+  region VARCHAR(20),
+  phone VARCHAR(20),
+  mileage INT
+);
